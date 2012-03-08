5b68eb0f1cdf4527493b975194bca69c4b348ab2
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-03-08 03:58:14 -0500

    Refinagle form layouts, they're identical now

--- src/clj/eastercalc/views/form.clj
+++ src/clj/eastercalc/views/form.clj
@@ -14,65 +14,111 @@
   [:div {:class "row"}
    [:h4 {:class "span12"} "Check all that apply."]
    [:form {:class "form-horizontal"
-          :action "/data/dates-for-years"
-          :method "GET"}
+           :action "/data/dates-for-years"
+           :method "GET"}
 
-   [:fieldset {:class "span3"}
-    [:div {:class "control-group"}
-     [:label {:class "control-label"
-              :for "eastern"}
-      "Orthodox Pascha"]
-     [:div {:class "controls"}
-      (form/check-box "eastern" true)]]
-    
-    [:div {:class "control-group"}
-     [:label {:class "control-label"
-              :for "western"}
-      "Western Easter"]
-     [:div {:class "controls"}
-      (form/check-box "western" false)]]]
+    [:fieldset {:class "span3"}
+     [:div {:class "control-group"}
+      [:label {:class "control-label"
+               :for "eastern"}
+       "Orthodox Pascha"]
+      [:div {:class "controls"}
+       (form/check-box "eastern" true)]]
+     
+     [:div {:class "control-group"}
+      [:label {:class "control-label"
+               :for "western"}
+       "Western Easter"]
+      [:div {:class "controls"}
+       (form/check-box "western" false)]]]
 
-   [:fieldset {:class "span4"}
-    [:div {:class "control-group"}
-     [:label {:class "control-label"
-              :for "year-start"}
-      "Start Year"]
-     [:div {:class "controls"}
-      [:input {:type "text"
-               :class "input-small"
-               :id "year-start"
-               :value (dt/year (dt/now))}]
-      
-      [:span {:class "help-inline"}
-       (help-tip {:title "Four-digit year, inclusive." :id "year-start-tip"}
-                 [:i {:class "icon-question-sign"}])]]]
-    [:div {:class "control-group"}
-     [:label {:class "control-label"
-              :for "year-end"}
-      "End Year"]
-     [:div {:class "controls"}
-      [:input {:type "text"
-               :class "input-small"
-               :id "year-end"
-               :value (dt/year (dt/plus (dt/now) (dt/years 1)))}]
-      [:span {:class "help-inline"}
-       (help-tip {:title "Four-digit year, inclusive." :id "year-end-tip"}
-                 [:i {:class "icon-question-sign"}])]]]]
-
+    [:fieldset {:class "span4"}
+     [:div {:class "control-group"}
+      [:label {:class "control-label"
+               :for "year-start"}
+       "Start Year"]
+      [:div {:class "controls"}
+       [:input {:type "text"
+                :class "span1"
+                :id "year-start"
+                :value (dt/year (dt/now))}]
+       
+       [:span {:class "help-inline"}
+        (help-tip {:title "Four-digit year, inclusive." :id "year-start-tip"}
+                  [:i {:class "icon-question-sign"}])]]]
+     [:div {:class "control-group"}
+      [:label {:class "control-label"
+               :for "year-end"}
+       "End Year"]
+      [:div {:class "controls"}
+       [:input {:type "text"
+                :class "span1"
+                :id "year-end"
+                :value (dt/year (dt/plus (dt/now) (dt/years 1)))}]
+       [:span {:class "help-inline"}
+        (help-tip {:title "Four-digit year, inclusive." :id "year-end-tip"}
+                  [:i {:class "icon-question-sign"}])]]]]]
+   
    [:div {:class "span4"}
-    [:div {:class "control-group"}
-     [:a {:class "btn btn-primary"
-          :id "submit-form"} "Generate!"]] 
-    [:div {:class "control-group"}
-     [:a {:class "btn btn-danger" :id "clear-form"} "Clear Form"]]]]])
+   [:div {:class "control-group"}
+    [:a {:class "btn btn-primary"
+         :id "submit-dates-for-years"} "Generate!"]] 
+   [:div {:class "control-group"}
+    [:a {:class "btn btn-danger" :id "clear-form"} "Clear Form"]]]])
 
 (defpartial years-for-dates-form []
-  [:h4 "Check all that apply:"]
-  [:p
-   (form/form-to [:get "/data/years-for-dates"])
-   (form/label "eastern" "Eastern Orthodox Pascha")
-   (form/check-box "eastern" true)
-   [:br]
-   (form/label "western" "Western Easter")
-   (form/check-box "western" false)])
+  [:div {:class "row"}
+   [:h4 {:class "span12"} "Check all that apply."]
+   [:form {:class "form-horizontal"
+           :action "/data/years-for-dates"
+           :method "GET"}
+
+    [:fieldset {:class "span3"}
+     [:div {:class "control-group"}
+      [:label {:class "control-label"
+               :for "eastern"}
+       "Orthodox Pascha"]
+      [:div {:class "controls"}
+       (form/check-box "eastern" true)]]
+     
+     [:div {:class "control-group"}
+      [:label {:class "control-label"
+               :for "western"}
+       "Western Easter"]
+      [:div {:class "controls"}
+       (form/check-box "western" false)]]]
+
+    [:fieldset {:class "span4"}
+     [:div {:class "control-group"}
+      [:label {:class "control-label"
+               :for "year-start"}
+       "Start Year"]
+      [:div {:class "controls"}
+       [:input {:type "text"
+                :class "span1"
+                :id "year-start"
+                :value (dt/year (dt/now))}]
+       [:span {:class "help-inline"}
+        (help-tip {:title "Four-digit year, inclusive." :id "year-start-tip"}
+                  [:i {:class "icon-question-sign"}])]]]
+     [:div {:class "control-group"}
+      [:label {:class "control-label"
+               :for "year-end"}
+       "End Year"]
+      [:div {:class "controls"}
+       [:input {:type "text"
+                :class "span1"
+                :id "year-end"
+                :value (dt/year (dt/plus (dt/now) (dt/years 1)))}]
+       [:span {:class "help-inline"}
+        (help-tip {:title "Four-digit year, inclusive." :id "year-end-tip"}
+                  [:i {:class "icon-question-sign"}])]]]]]
+
+   [:div {:class "span4"}
+   [:div {:class "control-group"}
+    [:a {:class "btn btn-primary"
+         :id "submit-years-for-dates"} "Generate!"]] 
+   [:div {:class "control-group"}
+    [:a {:class "btn btn-danger" :id "clear-form"} "Clear Form"]]]])
+
 
