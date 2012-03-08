c94ae9816847e6b9b7f4ee0a441a701db45521cd
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-03-08 03:58:31 -0500

    Add defpage for years-for-dates endpoint

--- src/clj/eastercalc/views/index.clj
+++ src/clj/eastercalc/views/index.clj
@@ -2,12 +2,20 @@
   (:require [eastercalc.views.common :as common])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]
-        [eastercalc.views.form :only [dates-for-years-form]]
+        [eastercalc.views.form :only [dates-for-years-form
+                                      years-for-dates-form]]
         [eastercalc.views.nav :only [nav]]))
 
 (defpage "/" []
-         (common/layout
-           (nav "/")
-           [:div {:class "row"}
-            [:div {:class "span12"} (dates-for-years-form)]
-            [:div {:id "results" :class "span12"}]]))
+  (common/layout
+   (nav "/")
+   [:div {:class "row"}
+    [:div {:class "span12"} (dates-for-years-form)]
+    [:div {:id "results" :class "span12"}]]))
+
+(defpage "/years-for-dates" []
+  (common/layout
+   (nav "/years-for-dates")
+   [:div {:class "row"}
+    [:div {:class "span12"} (years-for-dates-form)]
+    [:div {:id "results" :class "span12"}]]))
