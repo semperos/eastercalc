606d8a231571df97f39edfb109e8ec4d8e947147
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-03-08 03:57:20 -0500

    Make command-line interface to server more friendly

--- src/clj/eastercalc/server.clj
+++ src/clj/eastercalc/server.clj
@@ -1,11 +1,20 @@
 (ns eastercalc.server
+  (:use [clojure.tools.cli :only [cli]])
   (:require [noir.server :as server]))
 
 (server/load-views "src/clj/eastercalc/views/")
 
-(defn -main [& m]
-  (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+(defn -main [& args]
+  (let [[options args banner] (cli args
+                                   ["-e" "--environment" "Options: dev, prod" :default "dev"]
+                                   ["-p" "--port" "Application port" :default 8888 :parse-fn #(Integer. %)]
+                                   ["-h" "--help" "Show help" :default false :flag true])
+        mode (keyword (:environment options))
+        port (:port options)]
+    (when (:help options)
+      (println banner)
+      (System/exit 0))
     (server/start port {:mode mode
                         :ns 'eastercalc})))
+
 
