7d5c62fe1968db70db94d3edbce1950d5dbd19f8
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-03-08 03:57:50 -0500

    Here is where we implement the group-by to get years for given dates

--- src/clj/eastercalc/views/data.clj
+++ src/clj/eastercalc/views/data.clj
@@ -5,8 +5,10 @@
         clj-time.format
         [eastercalc.calc :only [pascha easter]]))
 
-(def custom-formatter
+(def month-day-formatter
   (formatter "MM/dd"))
+(def year-formatter
+  (formatter "yyyy"))
 
 (defpage [:get "/data/dates-for-years"]
   {:keys [year-start year-end western eastern] :as form}
@@ -18,8 +20,28 @@
                     :eastern (when-not (= eastern "false")
                                (for [year year-range]
                                  {:year year
-                                  :date (unparse custom-formatter (pascha year))}))
+                                  :date (unparse month-day-formatter (pascha year))}))
                     :western (when-not (= western "false")
                                (for [year year-range]
                                  {:year year
-                                  :date (unparse custom-formatter (easter year))}))})}))
+                                  :date (unparse month-day-formatter (easter year))}))})}))
+
+(defpage [:get "/data/years-for-dates"]
+  {:keys [date-start date-end year-start year-end western eastern] :as form}
+  (let [year-start (Integer. year-start)
+        year-end (Integer. year-end)
+        years (range year-start (inc year-end))]
+    {:headers {"Content-Type" "application/json"}
+     :body (encode {:disclaimer "All dates displayed according to the Gregorian calendar."
+                    :eastern (when-not (= eastern "false")
+                                (into (sorted-map)
+                                      (group-by (fn [year]
+                                                  (let [date (pascha year)]
+                                                    (str (.getMonthOfYear date) "/" (.getDayOfMonth date))))
+                                                years)))
+                    :western (when-not (= western "false")
+                                (into (sorted-map)
+                                      (group-by (fn [year]
+                                                  (let [date (easter year)]
+                                                    (str (.getMonthOfYear date) "/" (.getDayOfMonth date))))
+                                                years)))})}))
