335a400fa0631490b1a37273fb3f020d54a82f86
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-03-09 01:45:28 -0500

    Update data view to use remote instead of standard defpage

--- src/clj/eastercalc/views/data.clj
+++ src/clj/eastercalc/views/data.clj
@@ -1,7 +1,5 @@
 (ns eastercalc.views.data
-  (:use [noir.core :only [defpage]]
-        [noir.response :only [json]]
-        [cheshire.core :only [encode]]
+  (:use noir.fetch.remotes
         clj-time.format
         [eastercalc.calc :only [pascha easter month-of-year-str day-of-month-str]]))
 
@@ -10,36 +8,35 @@
 (def year-formatter
   (formatter "yyyy"))
 
-(defpage [:get "/data/dates-for-years"]
-  {:keys [year-start year-end western eastern] :as form}
+(defremote dates-for-years
+  [{:keys [year-start year-end western eastern] :as form}]
   (let [year-start (Integer. year-start)
         year-end (Integer. year-end)
         year-range (range year-start (inc year-end))]
-    {:headers {"Content-Type" "application/json"}
-     :body (encode {:disclaimer "All dates displayed according to the Gregorian calendar."
-                    :eastern (when-not (= eastern "false")
-                               (for [year year-range]
-                                 {:year year
-                                  :date (unparse month-day-formatter (pascha year))}))
-                    :western (when-not (= western "false")
-                               (for [year year-range]
-                                 {:year year
-                                  :date (unparse month-day-formatter (easter year))}))})}))
+    {"disclaimer" "All dates displayed according to the Gregorian calendar."
+     "eastern" (when-not (= eastern "false")
+                 (map (fn [year] {"year" year
+                                 "date" (unparse month-day-formatter (pascha year))})
+                      year-range))
+     "western" (when-not (= western "false")
+                 (map (fn [year] {"year" year
+                                 "date" (unparse month-day-formatter (easter year))})
+                      year-range))}))
 
-(defpage [:get "/data/years-for-dates"]
-  {:keys [date-start date-end year-start year-end western eastern] :as form}
-  (let [year-start (Integer. year-start)
-        year-end (Integer. year-end)
-        years (range year-start (inc year-end))]
-    {:headers {"Content-Type" "application/json"}
-     :body (encode {:disclaimer "All dates displayed according to the Gregorian calendar."
-                    :eastern (when-not (= eastern "false")
-                                (sort (group-by (fn [year]
-                                                  (let [date (pascha year)]
-                                                    (str (month-of-year-str date) "/" (day-of-month-str date))))
-                                                years)))
-                    :western (when-not (= western "false")
-                               (sort (group-by (fn [year]
-                                                 (let [date (easter year)]
-                                                   (str (month-of-year-str date) "/" (day-of-month-str date))))
-                                               years)))})}))
+;; (defpage [:get "/data/years-for-dates"]
+;;   {:keys [date-start date-end year-start year-end western eastern] :as form}
+;;   (let [year-start (Integer. year-start)
+;;         year-end (Integer. year-end)
+;;         years (range year-start (inc year-end))]
+;;     {:headers {"Content-Type" "application/json"}
+;;      :body (encode {:disclaimer "All dates displayed according to the Gregorian calendar."
+;;                     :eastern (when-not (= eastern "false")
+;;                                 (sort (group-by (fn [year]
+;;                                                   (let [date (pascha year)]
+;;                                                     (str (month-of-year-str date) "/" (day-of-month-str date))))
+;;                                                 years)))
+;;                     :western (when-not (= western "false")
+;;                                (sort (group-by (fn [year]
+;;                                                  (let [date (easter year)]
+;;                                                    (str (month-of-year-str date) "/" (day-of-month-str date))))
+;;                                                years)))})}))
