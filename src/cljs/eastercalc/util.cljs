c699a76405d28c31819881d1f4a0a536b39c29e4
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-03-09 01:45:59 -0500

    Move clj->js to util

---
+++ src/cljs/eastercalc/util.cljs
@@ -0,0 +1,16 @@
+(ns eastercalc.util)
+
+(defn clj->js
+  "Recursively transforms ClojureScript maps into Javascript objects,
+   other ClojureScript colls into JavaScript arrays, and ClojureScript
+   keywords into JavaScript strings.
+
+   Borrowed and updated from mmcgrana."
+  [x]
+  (cond
+    (string? x) x
+    (keyword? x) (name x)
+    (map? x) (.-strobj (reduce (fn [m [k v]]
+               (assoc m (clj->js k) (clj->js v))) {} x))
+    (coll? x) (apply array (map clj->js x))
+    :else x))
