270950b4667fed38b6c7c8586aa5db8b9080e02e
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-03-09 01:27:02 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eastercalc "0.1.0-SNAPSHOT"
+(defproject eastercalc "0.1.0"
             :description "Tools for calculating Easter and Pascha."
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojure/tools.cli "0.2.1"]
